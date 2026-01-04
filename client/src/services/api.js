80722b9ee06b9bdeb2b2ca93d71ca37124401804
Raúl S. <raulsimionidecarvalho@gmail.com>
2026-01-04 20:41:36 -0300

    Modificação api back

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const urlBackEnd = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const urlBackEnd = 'https://rskbuys-project.onrender.com/api'; 
 
 const api = axios.create({
   baseURL: urlBackEnd,
